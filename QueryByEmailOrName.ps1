751bacf7bc5de274699e812a41db0b9424767e0d
iagor ml <iagorluchesi@gmail.com>
2023-08-17 11:37:07 -0400

    final commit

---
+++ QueryByEmailOrName.ps1
@@ -0,0 +1,27 @@
+$emails = Get-Content Get-Content C:\path\txt.txt
+$names = Get-Content Get-Content C:\path\txt.txt
+
+function Get-Emails{
+ForEach ($users in $emails) {
+Get-ADUser -Filter "mail -eq '$users'" -SearchBase "OU=OrganizationalUnit,DC=example,DC=example" -Properties "mail"
+    }
+}
+
+function Get-By-Name{
+ForEach ($users_names in $names) {
+Get-ADUser -Filter "Name -eq '$users_names'" -SearchBase "OU=OrganizationalUnit,DC=example,DC=example"
+    }
+}
+
+Write-Host "type 1 to search by email or type 2 to search by name: " -NoNewline
+$anwser = Read-Host
+
+If($anwser -eq 1){
+Get-Emails
+}
+ElseIf($anwser -eq 2){
+Get-By-Name
+}
+Else{
+Write-Host "no action selected"
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# pwshell-ad
+this powershell script is able to get information about users by email or name
